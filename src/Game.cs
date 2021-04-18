a2503e02feb36272ba83325a762b416769c68517
Emik03 <emik.lakunishok@gmail.com>
2021-04-18 23:53:48 +0200

    Game: Added GetAllModPathsFromSource

--- src/Game.cs
+++ src/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Manager = Assets.Scripts.Settings.PlayerSettingsManager;
+using Player = Assets.Scripts.Settings.PlayerSettingsManager;
+using Manager = ModManager;
 using Info = Assets.Scripts.Mods.ModInfo;
 
 namespace KeepCoding.v14
@@ -44,12 +45,17 @@ namespace KeepCoding.v14
             /// <value>
             /// Gets all of the disabled mod paths.
             /// </value>
-            public static Func<List<string>> GetDisabledModPaths => global::ModManager.Instance.GetDisabledModPaths;
+            public static Func<List<string>> GetDisabledModPaths => Manager.Instance.GetDisabledModPaths;
 
             /// <value>
             /// Gets all of the enabled mod paths within the <see cref="ModSourceEnum"/> constraint.
             /// </value>
-            public static Func<ModSourceEnum, List<string>> GetEnabledModPaths => m => global::ModManager.Instance.GetEnabledModPaths((Info.ModSourceEnum)m);
+            public static Func<ModSourceEnum, List<string>> GetAllModPathsFromSource => source => Manager.Instance.GetAllModPathsFromSource((Info.ModSourceEnum)source);
+
+            /// <value>
+            /// Gets all of the enabled mod paths within the <see cref="ModSourceEnum"/> constraint.
+            /// </value>
+            public static Func<ModSourceEnum, List<string>> GetEnabledModPaths => source => Manager.Instance.GetEnabledModPaths((Info.ModSourceEnum)source);
         }
 
         /// <summary>
@@ -60,82 +66,82 @@ namespace KeepCoding.v14
             /// <value>
             /// Determines if vertical tilting is flipped or not.
             /// </value>
-            public static bool InvertTiltControls => Manager.Instance.PlayerSettings.InvertTiltControls;
+            public static bool InvertTiltControls => Player.Instance.PlayerSettings.InvertTiltControls;
 
             /// <summary>
             /// Determines if the option to lock the mouse to the window is enabled.
             /// </summary>
-            public static bool LockMouseToWindow => Manager.Instance.PlayerSettings.LockMouseToWindow;
+            public static bool LockMouseToWindow => Player.Instance.PlayerSettings.LockMouseToWindow;
 
             /// <summary>
             /// Determines if the option to show the leaderboards from the pamphlet.
             /// </summary>
-            public static bool ShowLeaderBoards => Manager.Instance.PlayerSettings.ShowLeaderBoards;
+            public static bool ShowLeaderBoards => Player.Instance.PlayerSettings.ShowLeaderBoards;
 
             /// <summary>
             /// Determines if the option to show the rotation of the User Interface is enabled.
             /// </summary>
-            public static bool ShowRotationUI => Manager.Instance.PlayerSettings.ShowRotationUI;
+            public static bool ShowRotationUI => Player.Instance.PlayerSettings.ShowRotationUI;
 
             /// <summary>
             /// Determines if the option to show scanlines is enabled.
             /// </summary>
-            public static bool ShowScanline => Manager.Instance.PlayerSettings.ShowScanline;
+            public static bool ShowScanline => Player.Instance.PlayerSettings.ShowScanline;
 
             /// <summary>
             /// Determines if the option to skip the title screen is enabled.
             /// </summary>
-            public static bool SkipTitleScreen => Manager.Instance.PlayerSettings.SkipTitleScreen;
+            public static bool SkipTitleScreen => Player.Instance.PlayerSettings.SkipTitleScreen;
 
             /// <value>
             /// Determines if the VR or regular controllers vibrate.
             /// </value>
-            public static bool RumbleEnabled => Manager.Instance.PlayerSettings.RumbleEnabled;
+            public static bool RumbleEnabled => Player.Instance.PlayerSettings.RumbleEnabled;
 
             /// <value>
             /// Determines if the touchpad controls are inverted.
             /// </value>
-            public static bool TouchpadInvert => Manager.Instance.PlayerSettings.TouchpadInvert;
+            public static bool TouchpadInvert => Player.Instance.PlayerSettings.TouchpadInvert;
 
             /// <summary>
             /// Determines if the option to always use mods is enabled.
             /// </summary>
-            public static bool UseModsAlways => Manager.Instance.PlayerSettings.UseModsAlways;
+            public static bool UseModsAlways => Player.Instance.PlayerSettings.UseModsAlways;
 
             /// <summary>
             /// Determines if the option to use parallel/simultaneous mod loading is enabled.
             /// </summary>
-            public static bool UseParallelModLoading => Manager.Instance.PlayerSettings.UseParallelModLoading;
+            public static bool UseParallelModLoading => Player.Instance.PlayerSettings.UseParallelModLoading;
 
             /// <summary>
             /// Determines if VR mode is requested.
             /// </summary>
-            public static bool VRModeRequested => Manager.Instance.PlayerSettings.VRModeRequested;
+            public static bool VRModeRequested => Player.Instance.PlayerSettings.VRModeRequested;
 
             /// <summary>
             /// The intensity of anti-aliasing currently on the game. Ranges 0 to 8.
             /// </summary>
-            public static int AntiAliasing => Manager.Instance.PlayerSettings.AntiAliasing;
+            public static int AntiAliasing => Player.Instance.PlayerSettings.AntiAliasing;
 
             /// <value>
             /// The current music volume from the dossier menu. Ranges 0 to 100.
             /// </value>
-            public static int MusicVolume => Manager.Instance.PlayerSettings.MusicVolume;
+            public static int MusicVolume => Player.Instance.PlayerSettings.MusicVolume;
 
             /// <summary>
             /// The current sound effects volume from the dosssier menu. Ranges 0 to 100.
             /// </summary>
-            public static int SFXVolume => Manager.Instance.PlayerSettings.SFXVolume;
+            public static int SFXVolume => Player.Instance.PlayerSettings.SFXVolume;
 
             /// <summary>
             /// Determines if VSync is on or off.
             /// </summary>
-            public static int VSync => Manager.Instance.PlayerSettings.VSync;
+            public static int VSync => Player.Instance.PlayerSettings.VSync;
 
             /// <value>
             /// The current language code.
             /// </value>
-            public static string LanguageCode => Manager.Instance.PlayerSettings.LanguageCode;
+            public static string LanguageCode => Player.Instance.PlayerSettings.LanguageCode;
         }
     }
 }
