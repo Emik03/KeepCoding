2caa6a85caed3309deaba42570aa2fd56dcba16f
Emik03 <emik.lakunishok@gmail.com>
2021-04-30 10:16:56 +0200

    WorkBase: Removed unnecessary access to MaximumThreadsActive

--- Source/Abstracts/WorkBase.cs
+++ Source/Abstracts/WorkBase.cs
@@ -12,7 +12,7 @@ namespace KeepCoding
         internal WorkBase(bool allowSimultaneousActive, uint maximumThreadsActive)
         {
             AllowSimultaneousActive = allowSimultaneousActive;
-            MaximumThreadsActive = maximumThreadsActive;
+            _maximumThreadsActive = maximumThreadsActive;
         }
 
         /// <summary>
@@ -25,9 +25,9 @@ namespace KeepCoding
 
             Thread.Interrupt();
 
-            IsRunning = false;
-
             ThreadsActive--;
+
+            IsRunning = false;
         }
 
         /// <value>
@@ -39,11 +39,6 @@ namespace KeepCoding
         /// Determines whether it should skip on starting a thread if it doesn't allow multiple.
         /// </value>
         public bool AllowSimultaneousActive { get; private protected set; }
-
-        /// <value>
-        /// Determines the maximum number of threads it allows to be running at once within this class. This will make the class wait until <see cref="ThreadsActive"/> is strictly less than this value.
-        /// </value>
-        private protected uint MaximumThreadsActive { get; set; }
 
         /// <value>
         /// Counts the number of threads currently active within this class, and all of its overloads.
@@ -58,13 +53,15 @@ namespace KeepCoding
         /// <summary>
         /// Waits until it is allowed to run a thread.
         /// </summary>
-        /// <returns><see cref="WaitWhile"/> until <see cref="ThreadsActive"/> is strictly less than <see cref="MaximumThreadsActive"/>.</returns>
-        private protected IEnumerator WaitForOtherThreads() => new WaitWhile(() => MaximumThreadsActive <= ThreadsActive);
+        /// <returns><see cref="WaitWhile"/> until <see cref="ThreadsActive"/> is strictly less than <see cref="_maximumThreadsActive"/>.</returns>
+        private protected IEnumerator WaitForOtherThreads() => new WaitWhile(() => _maximumThreadsActive <= ThreadsActive);
 
         /// <summary>
         /// Waits until its own thread is no longer running..
         /// </summary>
         /// <returns><see cref="WaitWhile"/> until <see cref="IsRunning"/> is no longer true.</returns>
         private protected IEnumerator WaitForOwnThread() => new WaitWhile(() => IsRunning);
+
+        private readonly uint _maximumThreadsActive;
     }
 }
