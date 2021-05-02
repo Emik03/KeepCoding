e0a47e279dcb35c64c9045d19b53d351bb6396d0
Emik03 <emik.lakunishok@gmail.com>
2021-05-02 03:27:27 +0200

    Sound: Added missing Equals and GetHashCode methods

--- Source/Instances/Sound.cs
+++ Source/Instances/Sound.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using static KMAudio;
+using static KMSoundOverride;
 
 namespace KeepCoding
 {
@@ -25,7 +27,7 @@ namespace KeepCoding
         /// An instance of sound where <see cref="Game"/> is defined.
         /// </summary>
         /// <param name="sound">The sound to insert.</param>
-        public Sound(KMSoundOverride.SoundEffect sound) => Game = sound; 
+        public Sound(SoundEffect sound) => Game = sound; 
 
         /// <summary>
         /// The custom sound, written out by name.
@@ -40,7 +42,7 @@ namespace KeepCoding
         /// <summary>
         /// The in-game sound.
         /// </summary>
-        public KMSoundOverride.SoundEffect? Game { get; private set; }
+        public SoundEffect? Game { get; private set; }
 
         /// <summary>
         /// An instance of Sound where <see cref="Custom"/> is defined.
@@ -61,7 +63,7 @@ namespace KeepCoding
         /// </summary>
         /// <param name="sound">The sound to insert.</param>
         /// <returns><see cref="Sound"/> with argument <paramref name="sound"/>.</returns>
-        public static implicit operator Sound(KMSoundOverride.SoundEffect sound) => new(sound);
+        public static implicit operator Sound(SoundEffect sound) => new(sound);
 
         /// <summary>
         /// Returns <see cref="Custom"/> for the current variable.
@@ -75,14 +77,14 @@ namespace KeepCoding
         /// </summary>
         /// <param name="sound">The variable to grab the property from.</param>
         /// <returns><paramref name="sound"/>'s <see cref="Game"/>.</returns>
-        public static explicit operator KMSoundOverride.SoundEffect?(Sound sound) => sound.Game;
+        public static explicit operator SoundEffect?(Sound sound) => sound.Game;
 
         /// <summary>
         /// Returns <see cref="Game"/> for the current variable.
         /// </summary>
         /// <param name="sound">The variable to grab the property from.</param>
         /// <returns><paramref name="sound"/>'s <see cref="Game"/>.</returns>
-        public static explicit operator KMSoundOverride.SoundEffect(Sound sound) => sound.Game.Value;
+        public static explicit operator SoundEffect(Sound sound) => sound.Game.Value;
 
         /// <summary>
         /// Stops the <see cref="Reference"/>'s sound.
@@ -92,8 +94,34 @@ namespace KeepCoding
         /// <summary>
         /// Determines if both <see cref="Sound"/> variables are equal.
         /// </summary>
+        /// <param name="obj">The comparison.</param>
+        /// <returns>True if <see cref="Custom"/>, <see cref="Reference"/>, and <see cref="Game"/> are equal.</returns>
+        public override bool Equals(object obj) => Equals(obj as Sound);
+
+        /// <summary>
+        /// Determines if both <see cref="Sound"/> variables are equal.
+        /// </summary>
         /// <param name="other">The comparison.</param>
         /// <returns>True if <see cref="Custom"/>, <see cref="Reference"/>, and <see cref="Game"/> are equal.</returns>
-        public bool Equals(Sound other) => Custom == other.Custom && Reference == other.Reference && Game == other.Game;
+        public bool Equals(Sound other) => other is not null && Custom == other.Custom && Reference == other.Reference && Game == other.Game;
+
+        /// <summary>
+        /// Gets the current hash code.
+        /// </summary>
+        /// <returns>The hash code of <see cref="Custom"/>, <see cref="Reference"/>, and <see cref="Game"/>.</returns>
+        public override int GetHashCode()
+        {
+            int hashCode = -675929889;
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Custom);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<KMAudioRef>.Default.GetHashCode(Reference);
+            hashCode = (hashCode * -1521134295) + Game.GetHashCode();
+            return hashCode;
+        }
+
+        /// <summary>
+        /// Converts the current sound to a string, returning the current sound.
+        /// </summary>
+        /// <returns><see cref="Game"/>, or if null, <see cref="Custom"/>.</returns>
+        public override string ToString() => Game.ToString() ?? Custom;
     }
 }
