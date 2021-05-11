efef279e5082228d6fe8a535fa7ebed924f4ca22
Emik03 <emik.lakunishok@gmail.com>
2021-05-11 13:44:12 +0200

    Added IModule interface

---
+++ Source/Interfaces/IModule.cs
@@ -0,0 +1,18 @@
+﻿namespace KeepCoding
+{
+    /// <summary>
+    /// Interface for regular and needy modded modules. Written by Emik.
+    /// </summary>
+    public interface IModule
+    {
+        /// <summary>
+        /// Called when the lights turn on.
+        /// </summary>
+        public void OnActivate();
+
+        /// <summary>
+        /// Called when the timer's seconds-digit changes.
+        /// </summary>
+        public void OnTimerTick();
+    }
+}
