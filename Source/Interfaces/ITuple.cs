944b7340819443e1b0e23cd79b9a713b2cc02b10
Emik03 <emik.lakunishok@gmail.com>
2021-05-11 13:44:19 +0200

    Added ITuple interface

---
+++ Source/Interfaces/ITuple.cs
@@ -0,0 +1,14 @@
+﻿
+namespace KeepCoding
+{
+    /// <summary>
+    /// Interface for tuple type. Written by Emik.
+    /// </summary>
+    public interface ITuple
+    {
+        /// <value>
+        /// All of the tuple's items as an array, ordered by item number.
+        /// </value>
+        public object[] ToArray { get; }
+    }
+}
