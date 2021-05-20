ddb40f64c70c6fffb1b2df6e33fea3e26b453f4d
Emik03 <emik.lakunishok@gmail.com>
2021-05-20 12:54:32 +0200

    ITuple: Added more properties

--- Source/Interfaces/ITuple.cs
+++ Source/Interfaces/ITuple.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace KeepCoding
 {
     /// <summary>
@@ -6,6 +7,31 @@ namespace KeepCoding
     /// </summary>
     public interface ITuple
     {
+        /// <summary>
+        /// Indexable tuple. Be careful when using this as the compiler will not notice if you are using the wrong type.
+        /// </summary>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        /// <exception cref="WrongDatatypeException"></exception>
+        /// <param name="i">The index to use.</param>
+        /// <returns>The item indexed into.</returns>
+        public object this[byte i] { get; set; }
+
+        /// <value>
+        /// Determines if the tuple data type is empty.
+        /// </value>
+        public bool IsEmpty { get; }
+
+        /// <value>
+        /// Gets the length of the tuple, describing the amount of elements there are.
+        /// </value>
+        public byte Length { get; }
+
+        /// <value>
+        /// Gets the upper bound of the tuple, which is the last index.
+        /// </value>
+        /// <exception cref="InvalidOperationException"></exception>
+        public byte UpperBound { get; }
+
         /// <value>
         /// All of the tuple's items as an array, ordered by item number.
         /// </value>
