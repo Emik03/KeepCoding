23ac8cb6822fa20357bb2b82adf55be5187f5fcd
Emik03 <emik.lakunishok@gmail.com>
2021-05-22 02:52:55 +0200

    ModBundle: Added sealed modifier

--- Source/MonoBehaviours/ModBundle.cs
+++ Source/MonoBehaviours/ModBundle.cs
@@ -5,7 +5,7 @@ namespace KeepCoding
     /// <summary>
     /// Mod bundle component. This is used to get information about the module at runtime, as this cannot be determined elsewhere. Written by Emik.
     /// </summary>
-    public class ModBundle : MonoBehaviour
+    public sealed class ModBundle : MonoBehaviour
     {
         /// <summary>
         /// The name of the bundle. This is required for the version number.
