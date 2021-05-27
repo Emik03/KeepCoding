152f1dc011c99134b7a286c08a46dd9e81ce121b
Emik03 <emik.lakunishok@gmail.com>
2021-05-27 12:28:04 +0200

    TPScript: Privated TwitchHelpMessage

--- Source/MonoBehaviours/TPScript.cs
+++ Source/MonoBehaviours/TPScript.cs
@@ -14,7 +14,8 @@ namespace KeepCoding
         /// <summary>
         /// The help message that gets sent when typing <c>!{0} help</c>.
         /// </summary>
-        public string TwitchHelpMessage;
+        [SerializeField]
+        private string TwitchHelpMessage;
 
         /// <value>
         /// Determines if it should allow for the timer to be skipped when the module it is in, as well as any other modules that would like to skip time, are the only unsolved modules left on the bomb. 
