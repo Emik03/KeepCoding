661393004a5e0e386eb4c4cf77fb0307e5279198
Emik03 <emik.lakunishok@gmail.com>
2021-06-11 01:54:11 +0200

    Sound: Suppress null

--- Source/Instances/Sound.cs
+++ Source/Instances/Sound.cs
@@ -130,8 +130,20 @@ namespace KeepCoding
         /// <param name="audio">The instance of <see cref="KMAudio"/> to play from.</param>
         /// <returns>A method that when called, will play the sound and return the <see cref="KMAudioRef"/> instance.</returns>
         public Func<Transform, bool, KMAudioRef> Method(KMAudio audio) =>
-            Custom is { } ? ((t, b) => audio.HandlePlaySoundAtTransformWithRef?.Invoke(Custom, t, b)) :
-            Game is { } ? (Func<Transform, bool, KMAudioRef>)((t, b) => b ? throw new ArgumentException("The game doesn't support looping in-game sounds.") : audio.HandlePlayGameSoundAtTransformWithRef?.Invoke(Game.Value, t)) :
+            Custom is { } ? ((t, b) => SuppressNull(() => audio.HandlePlaySoundAtTransformWithRef(Custom, t, b))) :
+            Game is { } ? (Func<Transform, bool, KMAudioRef>)((t, b) => b ? throw new ArgumentException("The game doesn't support looping in-game sounds.") : SuppressNull(() => audio.HandlePlayGameSoundAtTransformWithRef?.Invoke(Game.Value, t))) :
             throw new UnrecognizedValueException($"{this}'s properties {nameof(Custom)} and {nameof(Game)} are both null!");
+
+        private static KMAudioRef SuppressNull(Func<KMAudioRef> func)
+        {
+            try
+            {
+                return func();
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+        }
     }
 }
