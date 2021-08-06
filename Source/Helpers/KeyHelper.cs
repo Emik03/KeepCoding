1f767a7dc3335459f7cfe5766bb663cd914da121
Emik03 <emik.lakunishok@gmail.com>
2021-08-06 12:17:18 +0200

    KeyHelper: Added Yield<T>()

--- Source/Helpers/KeyHelper.cs
+++ Source/Helpers/KeyHelper.cs
@@ -642,5 +642,16 @@ namespace KeepCoding
 
             return output;
         }
+
+        /// <summary>
+        /// Wraps this object instance into an <see cref="IEnumerable{T}"/> consisting of a single item.
+        /// </summary>
+        /// <typeparam name="T">Type of the object.</typeparam>
+        /// <param name="item">The instance that will be wrapped.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> consisting of a single item.</returns>
+        public static IEnumerable<T> Yield<T>(this T item)
+        {
+            yield return item;
+        }
     }
 }
