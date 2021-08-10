102b0c80996a0efbbfd92c02543e8427630cd784
Emik03 <emik.lakunishok@gmail.com>
2021-08-10 02:38:56 +0200

    Reformatted XML documentation

--- Source/Instances/CustomComparer.cs
+++ Source/Instances/CustomComparer.cs
@@ -4,32 +4,32 @@ using System.Collections.Generic;
 namespace KeepCoding
 {
     /// <summary>
-    ///     Encapsulates an IComparer&lt;T&gt; that uses a comparison function provided as a delegate.</summary>
-    /// <typeparam name="T">
-    ///     The type of elements to be compared.</typeparam>
+    /// Encapsulates an IComparer&lt;T&gt; that uses a comparison function provided as a delegate.
+    /// </summary>
+    /// <typeparam name="T">The type of elements to be compared.</typeparam>
     public sealed class CustomComparer<T> : IComparer<T>
     {
         private readonly Comparison<T> _comparison;
 
         /// <summary>
-        ///     Constructor.</summary>
-        /// <param name="comparison">
-        ///     Provides the comparison function for this comparer.</param>
+        /// Constructor.
+        /// </summary>
+        /// <param name="comparison">Provides the comparison function for this comparer.</param>
         public CustomComparer(Comparison<T> comparison) => _comparison = comparison;
 
         /// <summary>
-        ///     Compares two elements.</summary>
+        /// Compares two elements.
+        /// </summary>
         /// <remarks>
-        ///     This method implements <see cref="IComparer&lt;T&gt;.Compare(T,T)"/>.</remarks>
+        /// This method implements <see cref="IComparer&lt;T&gt;.Compare(T,T)"/>.
+        /// </remarks>
         public int Compare(T x, T y) => _comparison(x, y);
 
         /// <summary>
-        ///     Creates and returns a CustomComparer which compares items by comparing the results of a selector function.</summary>
-        /// <param name="selector">
-        ///     Function selecting the actual value to be compared.</param>
-        /// <param name="comparer">
-        ///     Comparer to use for comparing the results of the selector function. If null, the default comparer is used;
-        ///     this comparer will use the IComparable interface if implemented.</param>
+        /// Creates and returns a CustomComparer which compares items by comparing the results of a selector function.
+        /// </summary>
+        /// <param name="selector">Function selecting the actual value to be compared.</param>
+        /// <param name="comparer">Comparer to use for comparing the results of the selector function. If null, the default comparer is used; this comparer will use the IComparable interface if implemented.</param>
         public static CustomComparer<T> By<TBy>(Func<T, TBy> selector, IComparer<TBy> comparer = null)
         {
             if (selector is null)
@@ -42,22 +42,19 @@ namespace KeepCoding
         }
 
         /// <summary>
-        ///     Creates and returns a CustomComparer which compares items by comparing the results of a selector function.</summary>
-        /// <param name="selector">
-        ///     Function selecting the actual value to be compared.</param>
-        /// <param name="comparison">
-        ///     Comparison to use for comparing the results of the selector function.</param>
+        /// Creates and returns a CustomComparer which compares items by comparing the results of a selector function.
+        /// </summary>
+        /// <param name="selector">Function selecting the actual value to be compared.</param>
+        /// <param name="comparison">Comparison to use for comparing the results of the selector function.</param>
         public static CustomComparer<T> By<TBy>(Func<T, TBy> selector, Comparison<TBy> comparison) => selector is null ? throw new ArgumentNullException(nameof(selector))
                 : comparison is null ? throw new ArgumentNullException(nameof(comparison))
                 : new CustomComparer<T>((a, b) => comparison(selector(a), selector(b)));
 
         /// <summary>
-        ///     Creates and returns a CustomComparer which compares items by comparing the results of a selector function.</summary>
-        /// <param name="selector">
-        ///     Function selecting the string value to be compared.</param>
-        /// <param name="ignoreCase">
-        ///     If false, an invariant culture string comparison is used. Otherwise, an ordinal no-case comparison (suitable
-        ///     for filenames etc).</param>
+        /// Creates and returns a CustomComparer which compares items by comparing the results of a selector function.
+        /// </summary>
+        /// <param name="selector">Function selecting the string value to be compared.</param>
+        /// <param name="ignoreCase">If false, an invariant culture string comparison is used. Otherwise, an ordinal no-case comparison (suitable for filenames etc).</param>
         public static CustomComparer<T> By(Func<T, string> selector, bool ignoreCase)
         {
             if (selector is null)
@@ -69,13 +66,10 @@ namespace KeepCoding
         }
 
         /// <summary>
-        ///     Creates and returns a CustomComparer which uses the current comparer first, and if the current comparer says
-        ///     the items are equal, further compares items by comparing the results of a selector function.</summary>
-        /// <param name="selector">
-        ///     Function selecting the actual value to be compared.</param>
-        /// <param name="comparer">
-        ///     Comparer to use for comparing the results of the selector function. If null, the default comparer is used;
-        ///     this comparer will use the IComparable interface if implemented.</param>
+        /// Creates and returns a CustomComparer which uses the current comparer first, and if the current comparer says the items are equal, further compares items by comparing the results of a selector function.
+        /// </summary>
+        /// <param name="selector">Function selecting the actual value to be compared.</param>
+        /// <param name="comparer">Comparer to use for comparing the results of the selector function. If null, the default comparer is used; this comparer will use the IComparable interface if implemented.</param>
         public CustomComparer<T> ThenBy<TBy>(Func<T, TBy> selector, IComparer<TBy> comparer = null)
         {
             if (selector is null)
@@ -92,12 +86,10 @@ namespace KeepCoding
         }
 
         /// <summary>
-        ///     Creates and returns a CustomComparer which uses the current comparer first, and if the current comparer says
-        ///     the items are equal, further compares items by comparing the results of a selector function.</summary>
-        /// <param name="selector">
-        ///     Function selecting the actual value to be compared.</param>
-        /// <param name="comparison">
-        ///     Comparison to use for comparing the results of the selector function.</param>
+        /// Creates and returns a CustomComparer which uses the current comparer first, and if the current comparer says the items are equal, further compares items by comparing the results of a selector function.
+        /// </summary>
+        /// <param name="selector">Function selecting the actual value to be compared.</param>
+        /// <param name="comparison">Comparison to use for comparing the results of the selector function.</param>
         public CustomComparer<T> ThenBy<TBy>(Func<T, TBy> selector, Comparison<TBy> comparison) => selector is null ? throw new ArgumentNullException(nameof(selector)) : new CustomComparer<T>((a, b) =>
         {
             int result = Compare(a, b);
@@ -105,13 +97,10 @@ namespace KeepCoding
         });
 
         /// <summary>
-        ///     Creates and returns a CustomComparer which uses the current comparer first, and if the current comparer says
-        ///     the items are equal, further compares items by comparing the results of a string selector function.</summary>
-        /// <param name="selector">
-        ///     Function selecting the actual value to be compared.</param>
-        /// <param name="ignoreCase">
-        ///     If false, an invariant culture string comparison is used. Otherwise, an ordinal no-case comparison (suitable
-        ///     for filenames etc).</param>
+        /// Creates and returns a CustomComparer which uses the current comparer first, and if the current comparer says the items are equal, further compares items by comparing the results of a string selector function.
+        /// </summary>
+        /// <param name="selector">Function selecting the actual value to be compared.</param>
+        /// <param name="ignoreCase">If false, an invariant culture string comparison is used. Otherwise, an ordinal no-case comparison (suitable for filenames etc).</param>
         public CustomComparer<T> ThenBy(Func<T, string> selector, bool ignoreCase)
         {
             if (selector is null)
@@ -128,9 +117,9 @@ namespace KeepCoding
     }
 
     /// <summary>
-    ///     Encapsulates an IEqualityComparer&lt;T&gt; that uses an equality comparison function provided as a delegate.</summary>
-    /// <typeparam name="T">
-    ///     The type of elements to be compared for equality.</typeparam>
+    /// Encapsulates an IEqualityComparer&lt;T&gt; that uses an equality comparison function provided as a delegate.
+    /// </summary>
+    /// <typeparam name="T">The type of elements to be compared for equality.</typeparam>
     public sealed class CustomEqualityComparer<T> : IEqualityComparer<T>
     {
         private readonly Func<T, T, bool> _comparison;
@@ -138,11 +127,10 @@ namespace KeepCoding
         private readonly Func<T, int> _getHashCode;
 
         /// <summary>
-        ///     Constructor.</summary>
-        /// <param name="comparison">
-        ///     Provides the comparison function for this equality comparer.</param>
-        /// <param name="getHashCode">
-        ///     Provides the hash function for this equality comparer.</param>
+        /// Constructor.
+        /// </summary>
+        /// <param name="comparison">Provides the comparison function for this equality comparer.</param>
+        /// <param name="getHashCode">Provides the hash function for this equality comparer.</param>
         public CustomEqualityComparer(Func<T, T, bool> comparison, Func<T, int> getHashCode)
         {
             _comparison = comparison;
@@ -150,46 +138,42 @@ namespace KeepCoding
         }
 
         /// <summary>
-        ///     Constructor which re-uses the default hash function. Use this overload only if using the objects’ original
-        ///     hash function is appropriate for this equality comparison.</summary>
-        /// <param name="comparison">
-        ///     Provides the comparison function for this equality comparer.</param>
+        /// Constructor which re-uses the default hash function. Use this overload only if using the objects’ original hash function is appropriate for this equality comparison.</summary>
+        /// <param name="comparison">Provides the comparison function for this equality comparer.</param>
         public CustomEqualityComparer(Func<T, T, bool> comparison) : this(comparison, null) { }
 
         /// <summary>
-        ///     Compares two elements for equality.</summary>
+        /// Compares two elements for equality.
+        /// </summary>
         /// <remarks>
-        ///     This method implements <see cref="IEqualityComparer&lt;T&gt;.Equals(T,T)"/>.</remarks>
+        /// This method implements <see cref="IEqualityComparer&lt;T&gt;.Equals(T,T)"/>.
+        /// </remarks>
         public bool Equals(T x, T y) => _comparison(x, y);
 
         /// <summary>
-        ///     Returns a hash code for an element.</summary>
+        /// Returns a hash code for an element.
+        /// </summary>
         /// <remarks>
-        ///     This method implements <see cref="IEqualityComparer&lt;T&gt;.GetHashCode(T)"/>.</remarks>
+        /// This method implements <see cref="IEqualityComparer&lt;T&gt;.GetHashCode(T)"/>.
+        /// </remarks>
         public int GetHashCode(T obj) => _getHashCode == null ? obj.GetHashCode() : _getHashCode(obj);
 
         /// <summary>
-        ///     Creates and returns an equality comparer that compares the equality of objects by comparing the equality of
-        ///     the result of a selector function.</summary>
-        /// <param name="selector">
-        ///     Function selecting the actual value to be compared.</param>
-        /// <param name="comparer">
-        ///     Comparer to use for comparing the results of the selector function.</param>
+        /// Creates and returns an equality comparer that compares the equality of objects by comparing the equality of
+        /// the result of a selector function.</summary>
+        /// <param name="selector">Function selecting the actual value to be compared.</param>
+        /// <param name="comparer">Comparer to use for comparing the results of the selector function.</param>
         public static CustomEqualityComparer<T> By<TBy>(Func<T, TBy> selector, IEqualityComparer<TBy> comparer) => selector is null ? throw new ArgumentNullException(nameof(selector))
                 : comparer is null ? throw new ArgumentNullException(nameof(comparer))
                 : new CustomEqualityComparer<T>((a, b) => comparer.Equals(selector(a), selector(b)), a => comparer.GetHashCode(selector(a)));
 
         /// <summary>
-        ///     Creates and returns an equality comparer that compares the equality of objects by comparing the equality of
-        ///     the result of a selector function.</summary>
-        /// <param name="selector">
-        ///     Function selecting the actual value to be compared.</param>
-        /// <param name="comparison">
-        ///     Function used to compare values for equality. If null, will use IEquatable if implemented, or the object's
-        ///     Equals override.</param>
-        /// <param name="getHashCode">
-        ///     Function used to compute hash codes. If null, will use IEquatable if implemented, or the object's GetHashCode
-        ///     override.</param>
+        /// Creates and returns an equality comparer that compares the equality of objects by comparing the equality of
+        /// the result of a selector function.
+        /// </summary>
+        /// <param name="selector">Function selecting the actual value to be compared.</param>
+        /// <param name="comparison">Function used to compare values for equality. If null, will use IEquatable if implemented, or the object's Equals override.</param>
+        /// <param name="getHashCode">Function used to compute hash codes. If null, will use IEquatable if implemented, or the object's GetHashCode override.</param>
         public static CustomEqualityComparer<T> By<TBy>(Func<T, TBy> selector, Func<TBy, TBy, bool> comparison = null, Func<TBy, int> getHashCode = null)
         {
             if (selector is null)
@@ -209,13 +193,11 @@ namespace KeepCoding
         }
 
         /// <summary>
-        ///     Creates and returns an equality comparer that compares the equality of objects by comparing the equality of
-        ///     the result of a string selector function.</summary>
-        /// <param name="selector">
-        ///     Function selecting the actual string value to be compared.</param>
-        /// <param name="ignoreCase">
-        ///     If false, an invariant culture string comparison is used. Otherwise, an ordinal no-case comparison (suitable
-        ///     for filenames etc).</param>
+        /// Creates and returns an equality comparer that compares the equality of objects by comparing the equality of
+        /// the result of a string selector function.
+        /// </summary>
+        /// <param name="selector">Function selecting the actual string value to be compared.</param>
+        /// <param name="ignoreCase">If false, an invariant culture string comparison is used. Otherwise, an ordinal no-case comparison (suitable for filenames etc).</param>
         public static CustomEqualityComparer<T> By(Func<T, string> selector, bool ignoreCase) => selector is null ? throw new ArgumentNullException(nameof(selector)) : By(selector, ignoreCase ? StringComparer.OrdinalIgnoreCase : StringComparer.InvariantCulture);
     }
 }
