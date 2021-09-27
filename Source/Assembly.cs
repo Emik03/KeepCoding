d60facbf8c11b573b436176134e037efacc14bae
Emik03 <emik.lakunishok@gmail.com>
2021-09-27 18:13:13 +0200

    Assembly: Version bump

--- Source/Assembly.cs
+++ Source/Assembly.cs
@@ -2,5 +2,5 @@
 using System.Reflection;
 
 [assembly: AssemblyDescription("KeepCoding is a C# class library by Emik (https://github.com/Emik03) meant to help speed up the process of scripting Keep Talking and Nobody Explodes modules by giving better alternatives to common commands, and automating things universal to module-creation. (such as moduleIds, or logging)")]
-[assembly: AssemblyVersion("11")]
+[assembly: AssemblyVersion("11.0.1")]
 [assembly: CLSCompliant(true)]
